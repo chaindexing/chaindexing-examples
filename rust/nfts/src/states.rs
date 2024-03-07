3f7d9f263ab1d768887133d87a1fac0295b66568
Joshua Oladele <jurshsmith@gmail.com>
2024-03-07 20:15:56 +0100

    Upgrade chaindexing version
    API Change: Shared state - default unit type
    for simplicity sakes in rust's NFT example

--- rust/nfts/src/states.rs
+++ rust/nfts/src/states.rs
@@ -2,24 +2,24 @@ use chaindexing::{ContractState, ContractStateMigrations};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct NftState {
+pub struct Nft {
     pub token_id: i32,
     pub contract_address: String,
     pub owner_address: String,
 }
 
-impl ContractState for NftState {
+impl ContractState for Nft {
     fn table_name() -> &'static str {
-        "nft_states"
+        "nfts"
     }
 }
 
-pub struct NftStateMigrations;
+pub struct NftMigrations;
 
-impl ContractStateMigrations for NftStateMigrations {
+impl ContractStateMigrations for NftMigrations {
     fn migrations(&self) -> Vec<&'static str> {
         vec![
-            "CREATE TABLE IF NOT EXISTS nft_states (
+            "CREATE TABLE IF NOT EXISTS nfts (
                 token_id INTEGER NOT NULL,
                 contract_address TEXT NOT NULL,
                 owner_address TEXT NOT NULL
